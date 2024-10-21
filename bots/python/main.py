3ead2b2eb6003fec6bbddcb0909bea4db4c257f5
Bogdan <bolossev@mail.ru>
2024-10-21 17:36:18 +0300

    Update main.py

--- bots/python/main.py
+++ bots/python/main.py
@@ -1,1 +1,11 @@
+import telebot
+
+TOKEN = ''
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.send_message(message.chat.id, message.text)
+
+bot.polling(none_stop=True)
 
